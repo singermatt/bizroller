424a8c65c0650dba4625de734fb0ec299efa8846
singermatt <matt@bartaste.com>
2013-04-04 09:16:38 -0400

    reCaptcha contact

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -7,13 +7,24 @@ class ContactController < ApplicationController
   def create
     @message = Message.new(params[:message])
     
-    if @message.valid?
-      NotificationsMailer.new_message(@message).deliver
-      redirect_to(root_path, :notice => "Message was successfully sent.")
+    if verify_recaptcha
+        if @message.valid?
+            NotificationsMailer.new_message(@message).deliver
+            redirect_to(root_path, :notice => "Message was successfully sent.")
+        else
+            flash.now.alert = "Please fill in all fields."
+            render :new
+        end
     else
-      flash.now.alert = "Please fill all fields."
+    #  build_resource
+    #  clean_up_passwords(resource)
+      flash.delete(:recaptcha_error)
+      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
       render :new
     end
+
+
+
   end
 
 end
