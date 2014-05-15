40d925c9db9b351271ef5ae5ca9d6ce35cfd3b3d
singermatt <matt@bartaste.com>
2014-05-15 11:30:26 -0400

    fix migration column name

--- db/migrate/20140512140200_add_current_position_fields.rb
+++ db/migrate/20140512140200_add_current_position_fields.rb
@@ -1,5 +1,5 @@
 class AddCurrentPositionFields < ActiveRecord::Migration
   def change
-	add_column :users, :current_position_1, :text
+	add_column :users, :current_position_industry_1, :text
   end
 end
